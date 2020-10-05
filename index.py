ed71bc1484f33488b9701340eda71ab8a2d6ab02
Tyler <tylerrp06@gmail.com>
2020-10-05 18:26:19 +0000

    Added app configuration

--- index.py
+++ index.py
@@ -1,14 +1,46 @@
 import sys
-from tkinter import * 
+import tkinter as tk
+from tkinter import messagebox
 sys.path.insert(0, "./Questions")
 from Questions import easy as e
 from Questions import medium as m
 from Questions import hard as h
 
-window = Tk()
-window.title("Quiz App")
-window.geometry("300x200+10+20")
-window.mainloop()
+# App Rendering
+class mainUI(tk.Tk):
+  
+  def __init__(self, *args, **kwargs):
+    tk.Tk.__init__(self, *args, **kwargs)
+    self.title("Quiz App")
+    self.geometry("1000x500")
+    self.resizable(False, False)
+    self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    self.frames = {}
+    container = tk.Frame(self)
+    for F in (startPage):
+      page_name = F.__name__
+      frame = F(parent=container, controller=self)
+      self.frames[page_name] = frame
+      frame.grid(row=0, column=0, sticky="nsew")
+    self.show_frame("StartPage")
+
+  def on_closing(self):
+    if messagebox.askokcancel("Quit", "Are you sure?"):
+      self.destroy()
+  
+  def show_frame(self, page_name):
+    frame = self.frames[page_name]
+    frame.tkraise()
+
+class startPage(tk.Frame):
+  def __init__(self, parent, controller):
+    tk.Frame.__init__(self, parent)
+    self.controller = controller
+    label = tk.Label(self, text="        Home Page        ", font=self.controller.title_font,fg="#263942")
+    label.grid(row=0, sticky="ew")
+    
+
 
 def diffRedirect():
   difficulty()
@@ -62,4 +94,5 @@ def difficulty():
 
 
 if __name__ == '__main__':
-  difficulty()
+  app = mainUI()
+  app.mainloop()
